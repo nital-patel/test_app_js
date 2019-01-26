bf3890c73d3038ce001984423f91067a088a67db
Nital Patel <patelnital36@gmail.com>
2019-01-26 17:51:25 -0500

    add AddEventlistener button click and open paragraph

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,18 @@ function updateName() {
     let name = prompt('Enter a new name');
     para.textContent = 'Player 1: ' + name;
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    function createParagraph() {
+        let para = document.createElement('p');
+        para.textContent = 'you clicked the button!';
+        document.body.appendChild(para);
+
+    }
+    const buttons = document.querySelectorAll('button');
+
+    for (let i=0; i < buttons.length ; i++) {
+        buttons[i].addEventListener('click', createParagraph);
+    }
+
+});
