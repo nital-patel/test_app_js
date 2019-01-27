16e6b292cbcc75e4f32891528386d61837098e4e
Nital Patel <patelnital36@gmail.com>
2019-01-27 18:36:59 -0500

    Add input and label

--- scripts/script.js
+++ scripts/script.js
@@ -21,3 +21,15 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
 });
+
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.loworHi');
+
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessFiels = document.querySelector('.guessField');
+
+let guessCount = 1;
+let resetButton;
